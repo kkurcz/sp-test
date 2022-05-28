686ac8d46f635364169cd7203bb36d6d2bbd48bb
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2022-05-28 17:01:51 +0200

    add template

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
 app.load('https://prod.spline.design/0oa-tHwq3l5w4ITa/scene.splinecode');
 
+console.log("app.js")
 // import Alpine from 'alpinejs'
  
 // window.Alpine = Alpine
