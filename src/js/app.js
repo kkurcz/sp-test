0d7392a48109d277a3fdd18fc490d4eb4d12cf72
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2022-05-28 15:59:50 +0200

    add template

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import '../scss/app.scss';
-import Alpine from 'alpinejs'
+// import Alpine from 'alpinejs'
  
-window.Alpine = Alpine
+// window.Alpine = Alpine
  
-Alpine.start()
+// Alpine.start()
