0f611ff817965dbae3c8f5b4fecd8d1c9dbf8639
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2022-05-28 16:40:01 +0200

    add template

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,10 @@
 import '../scss/app.scss';
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/0oa-tHwq3l5w4ITa/scene.splinecode');
+
 // import Alpine from 'alpinejs'
  
 // window.Alpine = Alpine
